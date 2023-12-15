bbfe985135b7ff1878b1294b16c8a80b613c4157
ndouajmdev <m.ndoua@telecom.gouv.ci>
2023-12-15 02:52:40 +0000

    mise a jour avant deploiement

--- app/Imports/ArticleImport.php
+++ app/Imports/ArticleImport.php
@@ -29,14 +29,13 @@ class ArticleImport implements ToModel
         $article->source_uuid = $row[10];
         $article->entrepot_uuid = $row[11];
         $article->status = $row[12];
-        $article->packaging = $row[13];
-        $article->poid_tonne = $row[14];
-        $article->hauteur = $row[15];
-        $article->largeur = $row[16];
-        $article->volume = $row[17];
-        $article->longueur = $row[18];
-        $article->price_unitaire = $row[19];
-        $article->etat = $row[20];
+        $article->poid_tonne = $row[13];
+        $article->hauteur = $row[14];
+        $article->largeur = $row[15];
+        $article->volume = $row[16];
+        $article->longueur = $row[17];
+        $article->price_unitaire = $row[18];
+        $article->etat = $row[19];
 
         if($row[0] != null){
         $article->save();
